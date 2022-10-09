0143e663033333aba2562bb867dd139d2654d2f2
Богдан Трегубов <funbogdan2003@gmail.com>
2022-10-09 15:24:43 +0300

    second commit

--- app/src/main/java/com/example/client/client.java
+++ app/src/main/java/com/example/client/client.java
@@ -16,15 +16,15 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public class client extends AppCompatActivity {
 
     EditText tb_message;
     Button btn_send;
-    TextView tb_data;
+    EditText tb_serverData;
     DatagramSocket socket;
-    String strSendDatagram, strGetDatagram; //перменнты которые будут хранить запросы и ответы
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,65 +32,48 @@ public class client extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         try {
             socket = new DatagramSocket(1234);
+            System.out.println("Сокет открыт :]");
         }
         catch (SocketException e) {
+            System.out.println("Сокет не открыт :(");
             e.printStackTrace();
         }
         init();
     }
 
-    private void init() {
+    private void init(){
         btn_send = findViewById(R.id.btn_send);
-        tb_data = findViewById(R.id.tb_data);
+        tb_serverData = findViewById(R.id.tb_serverData);
         tb_message = findViewById(R.id.tb_message);
+    }
 
-
-        btn_send.setOnClickListener(new View.OnClickListener() {
+    public void onClickSend(View view) throws Exception {
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Handler handler = new Handler();
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        strSendDatagram = tb_message.getText().toString();
-                        sendMsg(strSendDatagram);
-                        tb_message.setText("");
-                        try {
-                            receiveMsg();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }).start();
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        tb_data.setText(strGetDatagram);
-                    }
-                });
+            public void run() {
+                try {
+                String str = tb_message.getText().toString();
+                byte[] bytes = str.getBytes();
+                InetAddress address = InetAddress.getByName("192.168.56.1");
+                DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, 5040);
+                socket.send(packet);
+                System.out.println("Данные отправлены :]");
+                tb_message.setText("");
+
+                byte[] bytes1 = new byte[1024];
+                DatagramPacket packet1 = new DatagramPacket(bytes1, bytes.length);
+                socket.receive(packet1);
+                System.out.println("Данные получены");
+                String str1 = new String(packet1.getData(), 0, packet1.getLength());
+                tb_serverData.setText(str1);
+                socket.close();
+
+                } catch (IOException e) {
+                    System.out.println("Ошибка получения или отпрвления данных :(");
+                    e.printStackTrace();
+                }
             }
-        });
+        }).start();
 
-    }
-
-
-    public void sendMsg(String strSendDatagram) {
-        try {
-            socket = new DatagramSocket();
-            InetAddress server = InetAddress.getByName("192.168.56.1");
-            byte[] data = strSendDatagram.getBytes();
-            DatagramPacket packet = new DatagramPacket(data, data.length, server, 5040);
-            socket.send(packet);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void receiveMsg() throws IOException {
-        byte[] data = new byte[1024];
-        DatagramPacket packet2 = new DatagramPacket(data, data.length);
-        socket.receive(packet2);
-        strGetDatagram = new String(packet2.getData(), 0, packet2.getLength());
-        System.out.println(strGetDatagram);
     }
 }
